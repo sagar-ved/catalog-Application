54317da831b375dff19a5e5f34e08caee912bd35
keller2019 <201951131@iiitvadodara.ac.in>
2022-10-12 00:05:02 +0530

    chapter3

---
+++ lib/Pages/Home_Page.dart
@@ -0,0 +1,26 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const int days = 30;
+    const String name = "Sagar ved";
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text("Catalog Application"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of Android! By $name 😀"),
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+    );
+  }
+}
